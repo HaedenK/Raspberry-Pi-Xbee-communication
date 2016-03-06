9256bf46c0390d1b9281bf057acd62491800706e
Benjamin <boboul.benjamin@outlook.fr>
2016-03-06 22:20:42 +0100

    somes updates

--- readAndSend.py
+++ readAndSend.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 import os, sys, time, fcntl, serial
+
+def printLog(logType, logMsg, logOnline):
+        logData = "[" + logType + "]: " + logMsg
+        print(logData)
+        if logOnline:
+                serial.writelines(logData)
+                pass
+        pass
+
 if __name__ == '__main__':
         temp = 0.0
         lum = 0.0
@@ -11,9 +20,8 @@ if __name__ == '__main__':
         serial.writeTimeout = 1
         serial.open()
         fcntl.fcntl(sys.stdin, fcntl.F_SETFL, os.O_NONBLOCK)
-        serial.writelines("[INFOS] Raspberry est ok")
-        print "[INFOS] Raspberry is up and running"
-        print "research xbee module, please wait..."
+        printLog("INFOS", "Raspberry is up and running", True)
+        printLog("INFOS", "research xbee module, please wait...", False)
 
         def processData(Input):
                 dataIn = Input.split('_')
@@ -25,19 +33,19 @@ if __name__ == '__main__':
                 while True:
                         line = serial.readline().decode('utf-8')
                         if line:
-                                print "[input] Arduino -> " + line
+                                printLog("INPUT", "Arduino -> " + line, False)
                                 processData(line)
-                                print "[INFOS] The temperature of the room is : " + temp + "C"
-                                print "[INFOS] The brightness of the room is : " + lum + "%"
-                                linin = sys.stdin.readline()
-                                serial.writelines(linin)
-                                if linin:
-                                        print("[output] data Send -> " +linin)
-                                        linin = false;
+                                printLog("INFOS", "The temperature of the room is : " + temp + "C", False)
+                                printLog("INFOS", "The luminosity of the room is : " + lum + "%", False)
+                        linin = sys.stdin.readline()
+                        serial.writelines(linin)
+                        if linin:
+                                print("[output] data Send -> " +linin)
+                                linin = false;
         except IOError:
                 time.sleep(0.1)
         except KeyboardInterrupt:
-                print("key interrupt", ERROR_TEXT)
+                print("key interrupt")
                 serial.writelines("[ERROR] RPI script down.")
         finally:
-                print "script down, please reboot."
+                print("script down, please reboot.")
